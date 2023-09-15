f8a1564a40549dfb6609c1abf9946f0a915da252
MarshmelloRice <xinxin99661@gmail.com>
2023-09-15 16:47:06 -0400

    Create makefile and finish part a

--- Homework3/alloc-mem-lastname.C
+++ Homework3/alloc-mem-lastname.C
@@ -1,25 +1,47 @@
+/*
+* alloc-mem-lastname.c / Memory Management
+*
+* My Name / CS5600 / Northeastern University
+* Fall 2023 / Sep 15, 2023
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
 
+// Allocate 1MB of stack memory.
+void allocateStackMemory() {
+    char stackMemory[1 * 1024 * 1024];
+    printf("1MB of stack memory is allocated at %p.\n", stackMemory);
+}
 
-static byte[] static_memory[5 * 1024];
+// Allocate 10MB of heap memory.
+char* alloatedHeapMemory() {
+    char* heapMemory = (char*)malloc(10 * 1024 * 1024);
+    printf("5MB of heap memory is allocated at %p.\n", heapMemory);
+    return heapMemory;
+}
 
-void main() {
+// Allocate 5MB of static memory.
+static char staticMemory[5 * 1024 * 1024];
+
+int main() {
+
+    printf("\n----------Stack----------\n");
+    allocateStackMemory();
+    printf("Stack memory is freed when the function exists.\n\n");
     
-    byte[] stack_memory[1024];
-    
-    byte[] heap_memory[10 * 1024];
-
-    if (heap_memory == NULL) {
-        printf("Error: Failed to allocate memory from the heap.\n");
-        exit(0);
+    printf("----------Heap----------\n");
+    char* heapMemory = alloatedHeapMemory();
+    if (heapMemory == NULL) {
+        printf("Error: Failed to allocate memory from the heap.\n\n");
+        return -1;
+    } else {
+        free(heapMemory);
+        printf("Heap memory is freed manually.\n\n");
     }
 
-    printf("Heap memory is allocated at %p.\n", heap_memory);
-    printf("Stack memory is allocated at %p.\n", stack_memory);
-    printf("Static memory is allocated at %p.\n", static_memory);
-
-    free(heap_memory);
-    printf("Heap memory is free.\n");
-
-    printf("Heap memory is allocated at %p.\n", heap_memory);
+    printf("----------Static Segment----------\n");
+    printf("Static memory is allocated at %p and is freed when the program terminates.\n\n", staticMemory);
+    
+    return 0;
 }
