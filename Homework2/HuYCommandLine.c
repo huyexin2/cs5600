5086ff562d8ceaf4ff3dad192cee71765647a33b
MarshmelloRice <xinxin99661@gmail.com>
2023-09-15 14:11:21 -0400

    Add comments and error code

--- Homework2/HuYCommandLine.c
+++ Homework2/HuYCommandLine.c
@@ -7,7 +7,9 @@
 
 #include <stdio.h>
 
+typedef void (*ConversionFunc)(char*);
 
+// Compare string
 int strcompre(char* str1, char* str2) {
     int flag = 0;
     while(*str1 != '\0' && *str2 != '\0') {
@@ -20,6 +22,7 @@ int strcompre(char* str1, char* str2) {
     return flag;
 }
 
+// Convert the word to uppercase.
 void lower2upper(char* c){
     while (*c != '\0'){
         if (*c >= 'a' && *c<= 'z'){
@@ -29,6 +32,7 @@ void lower2upper(char* c){
     }
 }
 
+// Convert the word to lowercase.
 void upper2lower(char* c){
     while (*c != '\0'){
         if (*c >= 'A' && *c<= 'Z'){
@@ -38,6 +42,7 @@ void upper2lower(char* c){
     }
 }
 
+// Convert the first letter capitalized and all other characters of the word in lower case.
 void cap(char* c){
     if (*c >= 'a' && *c<= 'z'){
         *c = *c-32;
@@ -54,29 +59,27 @@ void cap(char* c){
 
 int main(int argc, char* argv[])
 {
-    
-    if(strcompre(argv[1], "-u") == 0){
-        for (int i = 2; i < argc; i++){
-            lower2upper(argv[i]);
-            printf("%s ", argv[i]);
-        }
-        printf("\n");
-    }
-    if(strcompre(argv[1], "-l") == 0){
-        for (int i = 2; i < argc; i++){
-            upper2lower(argv[i]);
-            printf("%s ", argv[i]);
-        }
-        printf("\n");
-    }
-    if(strcompre(argv[1], "-cap") == 0){
-        for (int i = 2; i < argc; i++){
-            cap(argv[i]);
-            printf("%s ", argv[i]);
-        }
-        printf("\n");
+    if (argc < 3) {
+        printf("Please provide at least 3 arguments (program name, option, and at least one word)");
     }
     
+    ConversionFunc conversionFunc = NULL;
+
+    if (strcompre(argv[1], "-u") == 0) {
+        conversionFunc = lower2upper;
+    } else if (strcompre(argv[1], "-l") == 0) {
+        conversionFunc = upper2lower;
+    } else if (strcompre(argv[1], "-cap") == 0) {
+        conversionFunc = cap;
+    } else {
+        return -1;
+    }
+        
+    for (int i = 2; i < argc; i++){
+        conversionFunc(argv[i]);
+        printf("%s ", argv[i]);
+    }
+
+    printf("\n");
     return 0;
 }
-
