534fd097f1b393426fa23966d9b399666e17ef97
MarshmelloRice <xinxin99661@gmail.com>
2023-09-16 16:35:13 -0400

    finish part B and part C

--- Homework3/partB&C/load-mem-lastname.C
+++ Homework3/partB&C/load-mem-lastname.C
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Define the dynBlock structure.
 typedef struct {
     int* array;
-    size_t size;
+    int size;
 } dynBlock;
 
 // Allocate a single instance of a dynBlock from the heap and returns a point to the allocated object
-dynBlock* allocDynBlock() {
-    dynBlock* block = (dynBlock*)malloc(sizeof(dynBlock));
-    return block;
+dynBlock* allocDynBlock(int size) {
+    dynBlock* newBlock = (dynBlock*)malloc(sizeof(dynBlock));
+    if (newBlock == NULL) {
+        printf("Error: Memory allocation for new dynBlock failed.\n");
+    }
+
+    newBlock->size = size;
+    newBlock->array = (int*)malloc(size * sizeof(int));
+    if (newBlock->array == NULL) {
+        printf("Error: Memory allocation for array failed.\n");
+        free(newBlock);
+        return NULL;
+    }
+    return newBlock;
 }
 
 // Store an array of integers in a dynamic block.
-void storeMem2Blk(dynBlock* block, int* array, size_t size) {
-    for (size_t i = 0; i < size; i++) {
+void storeMem2Blk(dynBlock* block, int* array, int arraySize) {
+    for (int i = 0; i < arraySize; i++) {
         block->array[i] = array[i];
     }
 }
@@ -24,15 +36,36 @@ int main() {
     // Read file
     FILE *file;
     file = fopen("blocks.data", "r");
-     if (file != NULL) {
-        char buffer[100];
-        while(fgets(buffer, sizeof(buffer), file) != NULL) {
-            printf("%s", buffer);
-        }
-        fclose(file);
-    } else {
-        printf("Error: Cannot open the file.\n");
+
+     if (file == NULL) {
+        printf("Error: Cannot open blocks.data.\n");
         return -1;
+     }
+
+    char buffer[500];
+    while(fgets(buffer, sizeof(buffer), file) != NULL) {
+        int array[500];
+        int size = 0;
+        char* token = strtok(buffer, " ");
+        while (token != NULL) { 
+            sscanf(token, "%d", &array[size]);
+            size ++;
+            token = strtok(NULL, " ");
+        }
+        dynBlock* block = allocDynBlock(size);
+        storeMem2Blk(block, array, size);
+
+        // Print the array stored in the dynBlock
+        printf("\n--------dynBlock %p--------\n", block);
+        for (int i = 0; i < size; i++) {
+            printf("%d ", block->array[i]);
+        }
+        printf("\n");
+
+        // Free memory
+        free(block->array);
+        free(block);
     }
+    fclose(file);
     return 0;
 }
